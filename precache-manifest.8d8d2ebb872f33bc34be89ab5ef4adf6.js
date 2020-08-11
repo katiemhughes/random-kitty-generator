4e75368b6e8ca657c9ccf1efcd66c6e8d2b20a42
katiehughes <katie,m.hughes@hotmail.co.uk>
2020-08-11 12:07:35 +0100

    Updates

---
+++ precache-manifest.8d8d2ebb872f33bc34be89ab5ef4adf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38c83634842de1f1d6c9a455dd930a5",
+    "url": "/random-kitty-generator/index.html"
+  },
+  {
+    "revision": "f73258d0a70d878a6edc",
+    "url": "/random-kitty-generator/static/css/main.02ebfaae.chunk.css"
+  },
+  {
+    "revision": "20f8b1340986fdb8c337",
+    "url": "/random-kitty-generator/static/js/2.3ac9fd7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-kitty-generator/static/js/2.3ac9fd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73258d0a70d878a6edc",
+    "url": "/random-kitty-generator/static/js/main.7a99d938.chunk.js"
+  },
+  {
+    "revision": "4a2cd288ac5445f5f0cf",
+    "url": "/random-kitty-generator/static/js/runtime-main.1d0d4c00.js"
+  },
+  {
+    "revision": "2ea3ca683c2f86e92d43729eb3deaa72",
+    "url": "/random-kitty-generator/static/media/pawwallpaper.2ea3ca68.png"
+  }
+]);
